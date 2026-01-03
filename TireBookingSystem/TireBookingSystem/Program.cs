6cb3d57e1dbebe065aa6d2319dfca2b6d7d910f9
Theodor Herrdin <theodor.herrdin@telia.com>
2026-01-03 15:11:14 +0100

    Skapat ett nytt projekt för TireBookingSystem uppgiften.

---
+++ TireBookingSystem/TireBookingSystem/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace TireBookingSystem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
