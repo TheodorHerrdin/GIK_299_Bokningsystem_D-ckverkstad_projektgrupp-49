88c4dcad5c7aae3681829ede7a041749d665c93a
Theodor Herrdin <theodor.herrdin@telia.com>
2026-01-03 16:04:19 +0100

    Vi har skapat metoder och byggt menyn där användaren får göra sitt val för att gå vidare i programmet.

--- TireBookingSystem/TireBookingSystem/Booking.cs
+++ TireBookingSystem/TireBookingSystem/Booking.cs
@@ -6,7 +6,34 @@ using System.Threading.Tasks;
 
 namespace TireBookingSystem
 {
-    internal class Booking
+    public class Booking
     {
+        public static void SearchFreeTimes() 
+        {
+            Console.WriteLine("Här kommer lediga tider att visas senare i koden");
+            Console.WriteLine("Tryck på en valfri tangent för att gå vidare");
+            Console.ReadKey();
+        }
+
+        public static void AddBooking()
+        {
+            Console.WriteLine("Här kommer man kunna boka en tid senare");
+            Console.WriteLine("Tryck på en valfri tangent för att gå vidare");
+            Console.ReadKey();
+        }
+
+        public static void CancelBooking() 
+        {
+            Console.WriteLine("Här kommer tider att avbokas senare");
+            Console.WriteLine("Tryck på valfri tangent för att gå vidare");
+            Console.ReadKey();
+        }
+
+        public static void ListBookings()
+        {
+            Console.WriteLine("Här kommer alla bokningar att visas senare i koden");
+            Console.WriteLine("Tryck på valfri tangent för att gå tillbaka");
+            Console.ReadKey();
+        }
     }
 }
--- TireBookingSystem/TireBookingSystem/Program.cs
+++ TireBookingSystem/TireBookingSystem/Program.cs
@@ -4,7 +4,45 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            bool runProgram = true;
+
+            while (runProgram)
+            {
+                Console.WriteLine();
+                Console.WriteLine("Theo och Shahos Däckverkstad AB");
+                Console.WriteLine();
+                Console.WriteLine("Nummer 1: Sök efter lediga tider");
+                Console.WriteLine("Nummer 2: Lägg till en ny bokning");
+                Console.WriteLine("Nummer 3: Avboka din tid");
+                Console.WriteLine("Nummer 4: Visa alla bokningar");
+                Console.WriteLine("Nummer 5: Avsluta programmet");
+                Console.Write("Välj en siffra: ");
+
+                string choice = Console.ReadLine();
+
+                switch (choice)
+                {
+                    case "1":
+                        Booking.SearchFreeTimes();
+                        break;
+                    case "2":
+                        Booking.AddBooking();
+                        break;
+                    case "3":
+                        Booking.CancelBooking();
+                        break;
+                    case "4":
+                        Booking.ListBookings();
+                        break;
+                    case "5":
+                        runProgram = false;
+                        break;
+                    default:
+                        Console.WriteLine("Ogilitgt val, vänligen ange 1, 2 eller 3... Prova igen");
+                        break;
+
+                }
+            }
         }
     }
 }
